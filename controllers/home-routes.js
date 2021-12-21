16f7e466fe45a47318a47a0e01c862eba5b3803a
Iain <iainmilliken@gmail.com>
2021-12-21 16:14:22 -0700

    adding view and public elements

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 const { Post, User, Comment } = require('../models');
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
 
 router.get('/', (req, res) => {
   res.render('homepage');
